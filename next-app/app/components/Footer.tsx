01d0f7e9366b4a2d3fc9e44eb0fc6515c8aaf67b
Cole <colescarter@gmail.com>
2025-01-11 17:00:44 -0800

    Initial Commit

---
+++ next-app/app/components/Footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => (
+    <footer className="bg-white border-gray-300 py-6 text-center text-gray-600">
+      <div className="container mx-auto">
+        <p className="text-sm">&copy; {new Date().getFullYear()} Pants Index. All rights reserved.</p>
+        
+      </div>
+    </footer>
+  );
+  
+  export default Footer;
+  
---
+++ next-app/app/page.tsx
@@ -0,0 +1,19 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import ProductCard from './components/ProductCard';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+export default function Home() {
+  return (
+    <main>
+      <Header />
+      <h1 className='pt-32'>Hello World</h1>
+
+    
+      <Link href='/users'>Users</Link>
+      <ProductCard />
+      <Footer />
+    </main>
+  )
+}
