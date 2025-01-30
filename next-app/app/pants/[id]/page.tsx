147225b37660b6858ca15416a402a4744595b459
Cole <colescarter@gmail.com>
2025-01-29 21:42:36 -0800

    lol

--- next-app/app/pants/[id]/page.tsx
+++ next-app/app/pants/[id]/page.tsx
@@ -215,3 +215,17 @@
 // };
 
 // export default PantsDetailsPage;
+
+
+
+
+const PantsDetailPage = () => (
+  <footer className="bg-white border-gray-300 py-6 text-center text-gray-600">
+    <div className="container mx-auto">
+      <p className="text-sm">&copy; {new Date().getFullYear()} Pants Index. All rights reserved.</p>
+      
+    </div>
+  </footer>
+);
+
+export default PantsDetailPage;
