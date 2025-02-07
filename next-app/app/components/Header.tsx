1ba11caadb58205321e40feccdb0da14408614e9
Cole <colescarter@gmail.com>
2025-02-07 08:55:29 -0800

    Fixed banner

--- next-app/app/components/Header.tsx
+++ next-app/app/components/Header.tsx
@@ -10,14 +10,14 @@ interface HeaderProps {
 
 const Header: React.FC<HeaderProps> = () => (
   <header className="fixed top-0 left-0 right-0 z-50">
-    <div className=" hidden md:flex justify-center bg-red-600 text-white">
+    <div className=" hidden md:flex justify-center bg-red-600 text-white py-1">
         <Link href="/waitlist">We are currently developing! Click here to stay up to date or hit us with sugggestions!</Link>
     </div>
 
-    <div className="flex md:hidden justify-center bg-red-600 text-white">
+    <div className="flex md:hidden justify-center bg-red-600 text-white py-1">
         <Link href="/waitlist">Click here to stay up to date or hit us with sugggestions!</Link>
     </div>
-    <div className="fixed shadow top-6 left-0 right-0 flex justify-between lg:justify-center items-center pt-6 pb-3 px-5 bg-white  z-50">
+    <div className="fixed shadow top-8 left-0 right-0 flex justify-between lg:justify-center items-center pt-6 pb-3 px-5 bg-white  z-50">
       
       
       <h2 className={`text-3xl lg:text-4xl font-extrabold hover:text-red-600 ${GH.className}`}>
